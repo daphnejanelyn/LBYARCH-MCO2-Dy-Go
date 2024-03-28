04e3eedc7e0e74cc17136714556bdacd83e27236
Sealtiel Dy <sealtieldy74@gmail.com>
2024-03-28 21:45:09 +0800

    Removed printStencil function.

--- c_stencil.h
+++ c_stencil.h
@@ -5,13 +5,3 @@ void stencilC(size_t n, float* X, float* Y) {
         Y[i - 3] = X[i - 3] + X[i - 2] + X[i - 1] + X[i] + X[i + 1] + X[i + 2] + X[i + 3];
     }
 }
-
-void printStencil(size_t n, float* Y) {
-    for (size_t i = 0; i < n; i++) {
-        printf("%.2f", Y[i]);
-        if (i < n - 1) {
-            printf(",  ");
-        }
-    }
-    printf("\n\n");
-}
